2229f59d3a5bbd8caa31e3fcf5b4f0299510ee7b
segunlawal <lawalsegun72@gmail.com>
2023-04-29 18:42:14 -0700

    improve selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,11 +18,11 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		if (array[j] < array[min])
 			min = j;
-
-		temp = array[i];
-		array[i] = array[min];
-		array[min] = temp;
-		print_array(array, size);
+		if (min != i)
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
 
 	}
 }
