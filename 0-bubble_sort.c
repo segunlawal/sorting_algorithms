3ed6c5e77921021c06be3cfa59d1ced45bc3d4bd
segunlawal <lawalsegun72@gmail.com>
2023-04-29 19:08:49 -0700

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,29 +11,24 @@ void bubble_sort(int *array, size_t size)
 {
 	int temp;
 	size_t i, j;
-	bool isSorted, hasSwapped = false;
+	int swapped = 1;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size && swapped; i++)
 	{
-		isSorted = false;
+		swapped = 0;
 		for (j = i + 1; j < size; j++)
 		{
-			hasSwapped = false;
 			if (array[j] < array[i])
 			{
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
-				hasSwapped = true;
-				isSorted = true;
-			}
-			if (hasSwapped)
+				swapped = 1;
 				print_array(array, size);
-		}
-		if (!isSorted)
-			return;
+			}
+		}		
 	}
 }
